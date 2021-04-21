1eac549a698226ab334dd3d01629acfd1ddc4d93
felipe-basina <felipe.basina@gmail.com>
2021-04-21 14:15:56 -0300

    Adicionado projeto para implementacoes de algoritmos em C.

---
+++ c-alg/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void printMessage() {
+    char name[] = "felipe";
+    printf("Hello %s you're coding with C", name);
+}
+
+int main() {
+    printMessage();
+    return 0;
+}
