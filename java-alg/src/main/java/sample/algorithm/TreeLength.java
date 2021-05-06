9e18404bcb625fd4af4df94c68d59b06e7307bc4
felipe-basina <felipe.basina@gmail.com>
2021-05-06 20:57:46 -0300

    Adicionado exemplo de verificacao de tamanho de uma arvore binaria.

---
+++ java-alg/src/main/java/sample/algorithm/TreeLength.java
@@ -0,0 +1,40 @@
+package sample.algorithm;
+
+public class TreeLength {
+
+    public static void main(String[] args) {
+        System.out.println(treeLength(createNode()));
+    }
+
+    private static int treeLength(Node n) {
+        if (n == null) {
+            return 0;
+        }
+        int left = treeLength(n.left);
+        int right = treeLength(n.right);
+        return Math.max(left, right) + 1;
+    }
+
+    private static Node createNode() {
+        Node seven = new Node(7, null, null);
+        Node six = new Node(6, null, seven);
+        Node four = new Node(4, null, null);
+        Node five = new Node(5, four, six);
+        Node one = new Node(1, null, null);
+        Node two = new Node(2, one, null);
+        return new Node(3, two, five);
+    }
+
+}
+
+class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data, Node l, Node r) {
+        this.data = data;
+        this.left = l;
+        this.right = r;
+    }
+}
