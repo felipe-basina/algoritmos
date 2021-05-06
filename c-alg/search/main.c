05cd9cbf274f1ef1706ccea7f9f3fe25414c4bc4
felipe-basina <felipe.basina@gmail.com>
2021-05-06 09:51:34 -0300

    Adicionado comentarios + refatoracao.

--- c-alg/search/main.c
+++ c-alg/search/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include "search.h"
 
-int arr[16] = {3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 62};
-            // 0  1  2  3   4   5   6   7   8   9   10  11  12  13  14
-
-int binarySearch(int l, int h, int key) {
+/**
+ * Searches for key in the array with divide-conquer approach
+ * So it means at each search the array is divided by half
+ * The array must be sorted
+ *
+ * @param l
+ * @param h
+ * @param key
+ * @return l - the index of the found element in the array
+ */
+int binarySearch(int l, int h, int key, int arr[]) {
     if (l == h) {
         if (arr[l] == key) {
             return l;
@@ -14,18 +20,31 @@ int binarySearch(int l, int h, int key) {
     } else {
         int mid = (l + h) / 2;
         if (arr[mid] < key) {
-            return binarySearch(mid + 1, h, key);
+            return binarySearch(mid + 1, h, key, arr);
         } else {
-            return binarySearch(l, mid, key);
+            return binarySearch(l, mid, key, arr);
         }
     }
 }
 
-int main() {
+void doABinarySearch() {
+    int arr[16] = {3, 6, 8, 12, 14, 17, 25, 29, 31, 36, 42, 47, 53, 55, 62};
+                // 0  1  2  3   4   5   6   7   8   9   10  11  12  13  14
+
     char msg[] = "Searching for";
     int keys[] = {36, 3, 31, 13};
-    for (int index = 0; index < 4; index++) {
-        printf("%s [%2d] - index: %d\n", msg, keys[index], binarySearch(0, 16, keys[index]));
+    int totalKeys = (sizeof keys / sizeof (keys[0]));
+    int totalElements = (sizeof arr / sizeof (arr[0]));
+
+    for (int index = 0; index < totalKeys; index++) {
+        printf("%s [%2d] - index: %d\n",
+               msg,
+               keys[index],
+               binarySearch(0, totalElements, keys[index], arr));
     }
+}
+
+int main() {
+    doABinarySearch();
     return 0;
 }
