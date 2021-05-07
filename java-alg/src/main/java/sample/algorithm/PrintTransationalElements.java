d2d94a90716cfe17a337c3c9f85f850b0938976f
felipe-basina <felipe.basina@gmail.com>
2021-05-06 21:57:13 -0300

    Adicionado implementacao para imprimir valores transversal em arvore binaria.

--- java-alg/src/main/java/sample/algorithm/PrintTransationalElements.java
+++ java-alg/src/main/java/sample/algorithm/PrintTransationalElements.java
@@ -10,21 +10,51 @@ import java.util.Queue;
  * Where a node visits each of its children before visiting
  * any of their children's children
  */
-public class PrintTransactionalElements {
+public class PrintTransationalElements {
 
     public static void main(String[] args) {
-        Queue<Node> queue = new LinkedList<>();
-        System.out.println(queue.size());
-        queue.add(new Node(1, null, null));
-        queue.add(new Node(2, null, null));
-        queue.add(new Node(3, null, null));
-        System.out.println(queue.size());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.size());
+        printValues(createNode());
     }
 
+    private static void printValues(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        visit(root);
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            if (node.left != null) {
+                visit(node.left);
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                visit(node.right);
+                queue.add(node.right);
+            }
+        }
+    }
+
+    private static void visit(Node node) {
+        System.out.printf("%d ", node.data);
+    }
+
+/**
+// Representation
+//    1
+//     \
+//      2
+//       \
+//        5
+//      /   \
+//     3     6
+//      \
+//       4
+**/
     private static Node createNode() {
         Node four = new Node(4, null, null);
         Node six = new Node(6, null, null);
