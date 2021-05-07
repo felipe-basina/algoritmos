6881c44f6a30e721a5c62570a1762aacff2001ee
felipe-basina <felipe.basina@gmail.com>
2021-05-06 21:03:08 -0300

    Adicionado modelo para representacao de no (node).

---
+++ java-alg/src/main/java/sample/algorithm/model/Node.java
@@ -0,0 +1,13 @@
+package sample.algorithm.model;
+
+public class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data, Node l, Node r) {
+        this.data = data;
+        this.left = l;
+        this.right = r;
+    }
+}
