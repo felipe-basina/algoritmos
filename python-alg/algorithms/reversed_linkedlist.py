d3452a1e1889eac78fd06012833e54ed22a5c2e6
felipe-basina <felipe.basina@gmail.com>
2021-05-12 09:13:14 -0300

    Adicionado solucao final para inversao de lista ligada.

---
+++ python-alg/algorithms/reversed_linkedlist.py
@@ -0,0 +1,41 @@
+from algorithms.models.node import LinkedList
+
+'''
+Given the pointer to the head node of a linked list, change the next pointers of the nodes so that their order 
+is reversed. 
+The head pointer given may be null meaning that the initial list is empty.
+
+Example:
+1 -> 2 -> 3 -> 4 -> 5 -> NULL
+1 is the HEAD of the list
+'''
+
+
+def get_elements(node, nodes):
+    if node.next is None:
+        nodes.append(node)
+        return nodes
+    nodes.append(node)
+    return get_elements(node.next, nodes)
+
+
+def reverse(llist):
+    if llist is None:
+        return
+    nodes = []
+    elements = get_elements(llist, nodes)
+    new_ll = create_linked_list([el.data for el in reversed(elements)])
+    return new_ll
+
+
+def create_linked_list(values):
+    linked_list = LinkedList()
+    for val in values:
+        linked_list.insert_node(val)
+    return linked_list.head
+
+
+if __name__ == "__main__":
+    original_llist = create_linked_list([1, 2, 3, 4, 5])
+    new_llist = reverse(original_llist)
+    print("org_llist: {}\nnew_llist: {}".format(original_llist, new_llist))
