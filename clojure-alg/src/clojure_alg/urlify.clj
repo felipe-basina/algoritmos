b8f27d368f02fc93eb5ada6e56994bd3a831449f
felipe-basina <felipe.basina@gmail.com>
2021-05-17 22:15:40 -0300

    Adicionado docstring + refatoracao de parametro.

--- clojure-alg/src/clojure_alg/urlify.clj
+++ clojure-alg/src/clojure_alg/urlify.clj
@@ -7,23 +7,27 @@
 (defn split-string [string]
   (str/split string #""))
 
-(defn urlify [string]
+(defn urlify
+  "Replace all spaces with 20% token. It is not necessary to repeat
+  contiguous token"
+  [string]
   (loop [urlify-string ""
          previous-token? false
          string (split-string (str/trim string))]
     (if (empty? string)
       urlify-string
-      (let [character (first string)]
+      (let [character (first string)
+            rest-string (rest string)]
         (if (not (= character " "))
           (recur (str urlify-string character)
                  false
-                 (rest string))
+                 rest-string)
           (let [urlify-string-modified (if (not previous-token?)
                                          (str urlify-string "%20")
                                          urlify-string)]
             (recur urlify-string-modified
                    true
-                   (rest string))))))))
+                   rest-string)))))))
 
 (println (urlify "Mr John Smith     "))                     ; 'Mr%20John%20Smith'
 (println (urlify "  Minä    kirjoin    Clojure      "))     ; 'Minä%20kirjoin%20Clojure'
